ddc1aaaed57ac47721569dbd34575db3d1b2660a
changsuwei <you@example.com>
2017-11-09 04:59:05 +0800

    google_map_api

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express()
 const port =  41781// 請改成其他你喜歡的數字
 var data = {
-	placeid : "ChIJs-vv2xlVXTQR87cVUnPn-Es",
-	hot : 10.0,
-	category : 1.0,
-	lat : 25.0894062,
-	lng : 121.8475243,
-	name : "不厭亭",
-	description : ""
+	"placeid" : "ChIJs-vv2xlVXTQR87cVUnPn-Es",
+	"hot" : 5.0,
+	"category" : 1.0,
+	"lat" : 25.108914,
+	"lng" : 121.845868,
+	"name" : "九份開成殿管理委員會",
+	"description" : "九份夕陽 夜景 攝影點"
+
 }
-var json_data = JSON.stringify(data)
 app.get("/ajax_data", function(req, res) {
-  console.log("json!!!")
-  res.json(json_data)
+  console.log(data.hot)
+  res.json(data)
 })
 app.listen(port,() => {
     console.log(`Listening on port ${port}`)
