1865fb4ab0abe6a00b7d8e5dff967d8a13950692
hungyuhang <f74036093@mail.ncku.edu.tw>
2017-11-11 18:35:43 +0800

    add basic files of PWA

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+// WARN: this file must live in the app root because the scope for
+// service workers is defined by the directory in which the file resides.
+const cacheFile = [
+  './',
+  './index.html',
+  './css/round-about.css',
+  './js/map.js'
+]
+
+const cacheKey = 'demo-app-v1'
+
+// install
+self.addEventListener('install', event => {
+  console.log("now install")
+
+  event.waitUntil(
+    caches.open(cacheKey)
+    .then(cache => cache.addAll(cacheFile))
+    // .then(() => self.skipWaiting())
+  )
+})
+
+// activate
+self.addEventListener('activate', event => {
+  console.log(`activate ${cacheKey}, now ready to handle fetches`)
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      const promiseArr = cacheNames.map(item => {
+        if (item !== cacheKey) {
+          return caches.delete(item)
+        }
+      })
+      return Promise.all(promiseArr)
+    })
+  )
+})
+
+// fetch
+self.addEventListener('fetch', event => {
+  console.log(`${event.request.method}: ${event.request.url}`)
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
