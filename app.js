2f573d28b89ee0ac2a625d397c25443b71fba2dc
hsps955201 <hsps955201@gmail.com>
2017-11-16 01:10:22 +0800

    image load

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var app = express();
+var port = 7410;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://wp2017_groupi:wp2017_groupi@luffy.ee.ncku.edu.tw:27017/wp2017_groupi',{ useMongoClient: true });
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+app.use(express.static(__dirname + '/public'));
+/*app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});*/
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
+
+
+
+
+var multer  =   require('multer');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
+
