947390d7415a059d8fc4d0841756bc16a408bbb2
hsps955201 <hsps955201@gmail.com>
2017-11-18 16:43:32 +0800

    Update server.js
    1.目前資料庫有一個collection可以接收到使用者資料(type:text形式）
    2.另一個資料庫的collection存放照片路徑,時間,目前照片名字叫userPhoto + 使用者當下傳送時間

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ var options = {
         cert: fs.readFileSync('./ssl/certificate.crt'),
         };
 
+var multer = require('multer');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//mongoose
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;                                                                                                             
+mongoose.connect('mongodb://wp2017_groupi:wp2017_groupi@luffy.ee.ncku.edu.tw:27017/wp2017_groupi',{ useMongoClient: true });
+
 const app = express()
 const port = 41781
 
@@ -34,4 +45,71 @@ app.get("/ajax_data", function(req, res) {
 var server = https.createServer(options, app).listen(port, function(){
     console.log(`Listening on port ${port}`)
     app.use(express.static(__dirname + '/public'))
+});
+
+//save img path in mongodb(collections:files)
+//include originalname, destination, filename, path
+var imageSchema = new  mongoose.Schema({
+//  fieldname:String ,
+  originalname:String ,
+ // encoding:String ,
+ // mimetype:String ,
+  destination:String ,
+  filename: String,
+  path:String ,
+ // size: String
+          });
+var Image = mongoose.model('files', imageSchema);
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload');
+  },
+  filename: function (req, file, callback) {
+  callback(null, file.fieldname + '-' + moment().format('MMMM Do YYYY, h:mm:ss a'));
+  }
+});
+
+var upload = multer({ storage : storage}).single('userPhoto');
+app.post('/upload',function(req,res,next){
+   upload(req,res,function(err) {
+   var myimg = new Image(req.file);
+   myimg.save();
+
+   if(err) {
+      return res.end("Error uploading file.");
+           }
+      res.end("File is uploaded");
+      console.log(req.file);
+    });
+
+});
+
+// save user information in mongodb (collections:users)
+var nameSchema = new mongoose.Schema({                                                                                                         
+    topic:String,
+    time:String,
+//  options:String,  //1.即時2.作品
+//  comments:String
+    //User 經緯度
+    firstName: String,
+    lastName: String,
+   // path:String 
+});
+
+var User = mongoose.model("users", nameSchema);
+app.use(express.static(__dirname + '/public'));
+/*app.get("./", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});*/
+app.post('/addname', function(req, res) {
+   var myData = new User(req.body);
+   myData.save()
+     .then(item => {
+      res.send("Name saved to database");
+                   })
+
+      .catch(err => {
+       res.status(400).send("Unable to save to database");
+        });
+        console.log(req.body);
 });
