6286b65b9df3ebcb9a0e157a2500051a03bc5ab0
jason1534 <32288866+jason1534@users.noreply.github.com>
2017-11-20 09:17:21 -0600

    更改上傳圖片function

--- public/assets/js/upload.js
+++ public/assets/js/upload.js
@@ -39,12 +39,11 @@ $(function (){
 
             // 放置預覽元素後重設
             vs.root.find(".preview").html(vs.imgcode);
-            local_reset();
         }
 
         // 建立圖片
         var local_create_imgcode = function(key, src, callback){
-            vs.imgcode += '<img class="img" src="' + src + '">';
+            vs.imgcode += '<img class="img-responsive" src="' + src + '">';
             if (callback) callback();
         }
 
@@ -54,10 +53,77 @@ $(function (){
         }
 
         // 還原 input[type=file]
-        var local_reset = function (){
-            vs.imgcode = '';
-            vs.root.find(".upl").val(null);
-        }
+      
 
     });
 })
+
+// 獲取座標
+// var x=document.getElementById("demo");
+// function getLocation()
+//   {
+//   if (navigator.geolocation)
+//     {
+//     navigator.geolocation.watchPosition(showPosition);
+//     }
+//   else{x.innerHTML="Geolocation is not supported by this browser.";}
+//   }
+// function showPosition(position)
+//   {
+//   x.innerHTML="Latitude: " + position.coords.latitude + 
+//   "<br />Longitude: " + position.coords.longitude;  
+//   }
+ //獲取座標結束
+ 
+ //地圖
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+
+function showPosition(position)
+  {
+  lat=position.coords.latitude;
+  lon=position.coords.longitude;
+  latlon=new google.maps.LatLng(lat, lon)
+  mapholder=document.getElementById('mapholder')
+  mapholder.style.height='250px';
+  mapholder.style.width='500px';
+
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br />Longitude: " + position.coords.longitude;  
+
+  var myOptions={
+  center:latlon,zoom:14,
+  mapTypeId:google.maps.MapTypeId.ROADMAP,
+  mapTypeControl:false,
+  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  };
+  var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+  var marker=new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+  }
+
+function showError(error)
+  {
+  switch(error.code) 
+    {
+    case error.PERMISSION_DENIED:
+      x.innerHTML="User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML="Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML="The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML="An unknown error occurred."
+      break;
+    }
+  }
+
