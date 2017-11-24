9579f0b9aad10f5547a3849b94328b6d627903e4
changsuwei <you@example.com>
2017-11-25 00:44:26 +0800

    fix bug

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -58,15 +58,15 @@ function initMap() {
 
             // infowindow's html code
             var contentString = '<h1 style="color:black; text-align:center;">' + name + '</h1>' +
- +                '<p style="color:black; text-align:center;">' + description + '</p>' +
- +                '<div class="row">' +
- +                '<div class="col-sm-6">' +
- +                '<h2 style="color:black; text-align:center;"> 即時影像 </h2>' +
- +                '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + live_path + '>' + '</div>' +
- +                '<div class="col-sm-6">' +
- +                '<h2 style="color:black; text-align:center;"> 相關作品 </h2>' +
- +                '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + your_story_path + '>' + '</div>' +
-                  '</div>'
+                 '<p style="color:black; text-align:center;">' + description + '</p>' +
+                 '<div class="row">' +
+                 '<div class="col-sm-6">' +
+                 '<h2 style="color:black; text-align:center;"> 即時影像 </h2>' +
+                 '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + live_path + '>' + '</div>' +
+                 '<div class="col-sm-6">' +
+                 '<h2 style="color:black; text-align:center;"> 相關作品 </h2>' +
+                 '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + your_story_path + '>' + '</div>' +
+                 '</div>'
             // set the scene marker
             marker['infowindow'] = new google.maps.InfoWindow({
                 content: contentString
