1801aab08348cee9d898d0bc3c60610d3654ca2a
changsuwi <f74036255@ncku.edu.tw>
2017-11-25 00:37:29 +0800

    fix infowindow bug

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -31,14 +31,12 @@ function initMap() {
         position: center,
     });
 
+    var show_infowindow
     // get data from server
     get_json("map_initial", function(data) {
-
-
-        // initial only one infowindow to marker
-
-        var infoWindow = new google.maps.InfoWindow();
+        
         // decode json
+
         for (var k in data) {
             var placeid = data[k].placeid;
             var name = data[k].name;
@@ -60,28 +58,32 @@ function initMap() {
 
             // infowindow's html code
             var contentString = '<div>' +
-                '<h1 style="color:black; text-align:center;">' + name + '</h1>' +
-                '<p style="color:black; text-align:center;">' + description + '</p>' +
-                '<div class="row">' +
-                '<div class="col-sm-6">' +
-                '<h2 style="color:black; text-align:center;"> 即時影像 </h2>' +
-                '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + live_path + '>' + '</div>' +
-                '<div class="col-sm-6">' +
-                '<h2 style="color:black; text-align:center;"> 相關作品 </h2>' +
-                '<img class="img-responsive" style="width: 200px ; display:block; margin:auto;" src =' + your_story_path + '>' + '</div>' +
+                '<div>' +
                 '</div>' +
+                '<h1 style="color:black;">' + name + '</h1>' +
+                '<p style="color:black;">' + description + '</p>' +
+                '<img class="img-responsive" src =' + live_path +'>'  +
+                '<img src =' + your_story_path + 'class="img-responsive">' +
                 '</div>';
             // set the scene marker
-            
-
-
-            // set event listener and scene infowindow
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(contentString);
-                infoWindow.open(map, this);
+            marker['infowindow'] = new google.maps.InfoWindow({
+                content: contentString
             });
 
+            // set scene infowindow
+
+            google.maps.event.addListener(marker, 'click', function() {
+                if(typeof show_infowindow != 'undefined'){
+                    console.log(typeof show_infowindow)
+                    show_infowindow.close();                 
+                }
+                this['infowindow'].open(map, this);
+                show_infowindow = this['infowindow'];
+            });
+
+            /*google.maps.event.addListener(marker, 'mouseout', function() {
+                this['infowindow'].close();
+            });*/
         }
 
 
