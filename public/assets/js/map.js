158272d2115ac239f1e3bf78ceda8706b3ccc5f3
changsuwi <F74036255@mail.ncku.edu.tw>
2017-11-29 15:45:48 +0800

    map.html can catch user location

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -26,10 +26,21 @@ function initMap() {
         zoom: 14,
         center: center,
     });
-    var marker = new google.maps.Marker({
-        map: map,
-        position: center,
-    });
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
 
     // get data from server
     get_json("map_initial", function(data) {
